7096984ce815fc749915c65bd9e117f906b31db8
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-05 22:08:02 +0300

    adds requirements.txt

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Documents/django/instagram/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Documents/django/gallery/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
