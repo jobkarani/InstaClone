14a2796376e69f88985cab0edf09d6d2db3b3f9e
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-06 16:24:02 +0300

    creates profile model

--- insta/models.py
+++ insta/models.py
@@ -6,13 +6,15 @@ import datetime as dt
 
 # image
 class Image(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT,related_name='user_images')
     image = CloudinaryField('image')
     name = models.CharField(max_length=40)
     caption = models.CharField(max_length=200)
     posted_on = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=True,null=True, blank=True)
-    comments = models.IntegerField(blank=True)
+    comments = models.IntegerField(blank=True,null=True,default=True)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     
     class meta:
         ordering =['posted_on']
@@ -29,6 +31,24 @@ class Image(models.Model):
         
     def __str__(self):
         return self.name
+    
+class profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=650,blank=True,null=True)
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.save()
+        
+    def update(self):
+        self.save()
+        
+    def __str__(self):
+        return self.name
+    
     
         
     
