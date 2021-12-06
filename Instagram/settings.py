67e05f8388fd7f39c34681f012130b9848803f52
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-06 06:06:52 +0300

    installs bootstrap

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'insta.apps.InstaConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
