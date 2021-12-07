166bc360a83d0433bf1db05922dec80784b44e51
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-07 12:41:12 +0300

    creates a superuser

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^insta/',include('insta.urls'))
+    url(r'',include('insta.urls'))
 ]
--- insta/views.py
+++ insta/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image,profile
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to the Instagram')
+   posts = Image.objects.all()
+   return render(request, 'all-temps/index.html',{'posts':posts})
+
