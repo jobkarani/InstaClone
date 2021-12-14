c45351700a7c5db40e445bed05c504f6565f5cc9
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-14 14:49:53 +0300

    links profile name to userprofile

--- insta/urls.py
+++ insta/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path('comments/<image_id>', views.comments,name='comments'),
     path('like/',views.like_post, name='like_post'),
     path('profile/', views.profile, name='profile'),
-    path('user_profile/<user_id>', views.user_profile, name='user_profile'),
+    # path('user/<int:id>', views.user_profile, name='user.profile'),
+    path('user_profile/<user_id>', views.user_profile,name='user_profile'),
 ]
