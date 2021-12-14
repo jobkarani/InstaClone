53ae45aab31b7eaf87ea2eada56a5adecc07fc70
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-14 10:25:06 +0300

    makes migration changes

--- insta/tests.py
+++ insta/tests.py
@@ -48,4 +48,41 @@ class ImageTestCase(TestCase):
         image=Image.objects.get(name="init")
         self.assertEqual(image.name, "init")
         
+class LikeTestCase(TestCase):
+    def setUp(self):
+         user = User.objects.create(
+            username = 'ayubu',
+            first_name = 'ayub',
+            last_name = '254')
+         
+         Profile.objects.create(
+            bio = 'hey',
+            profile_photo = 'static/image/travel.webp',
+            user_id = user.id
+        )
+        
+         Image.objects.create(
+            name="init",
+            caption="ooops",
+            profile_id=user.id,
+            user_id=user.id
+        )
+         
+         Like.objects.create(
+             user_id = user.id,
+             image_id = image.id
+         )
+         
+    def test_image_id(self):
+         user = User.objects.create(
+            username = 'yub',
+            first_name = 'yubus',
+            last_name = '_254')
+         
+         Image.objects.create(
+            name="init",
+            caption="ooops",
+            profile_id=user.id,
+            user_id=user.id
+        )
     
