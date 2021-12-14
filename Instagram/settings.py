5b903915f13aad2bd4bcc15e4b0f7cc95341c92f
Job Karani <moringa@moringa-Thinkpad-L450>
2021-12-14 14:50:58 +0300

    adds padding to class card

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -144,11 +144,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+]
+
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
